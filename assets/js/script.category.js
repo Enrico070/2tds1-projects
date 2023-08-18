abe07c1b728786eb7eda0d77183e5c4b9dd6bedf
EnricoLona <enricolona564@gmail.com>
2023-08-18 11:05:09 -0300

    feat : create createProducts

--- assets/js/script.category.js
+++ assets/js/script.category.js
@@ -30,12 +30,42 @@ class CategoryService{
     }
 }
 
+class ProductService{
+   constructor(){
+    this.products = [];
+    this.nextProductId = 1;
+   }
+
+   addProduct(name , price, category){
+   const id = this.nextProductId;
+   this.nextProductId++;
+
+   const product = new Product(id, name, price, category);
+   this.products.push(product);
+   category.products.push(product);
+   }
+}
+
 const categoriesList = new CategoryService();
+const productsList = new ProductService();
+
 
 function createCategory(){
 const categoriesName = "candies";
 
 categoriesList.addCategory(categoriesName);
 
-console.log(categoriesList.categories);
+//console.log(categoriesList.categories);
+}
+
+
+function createProduct(){
+    const productName = "Choco";
+    const productPrice = 0.5;
+    const productCategory = categoriesList.categories[0];
+
+
+    productsList.addProduct(productName, productPrice, productCategory);
+
+    console.log( productsList.products);
 }
